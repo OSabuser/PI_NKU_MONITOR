047b310cc2060185d8a8739e1d147fec16edcb2a
Dmitry Akimov <von.akimov@gmail.com>
2022-10-13 11:56:18 +0300

    # 6
    Изменения в способе отрисовки

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from pyglet.gl import *
 import serial
 
 if __name__ == '__main__':
-
     # Set alpha blending config
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
@@ -32,72 +31,33 @@ if __name__ == '__main__':
     ser = serial.Serial(port='/dev/ttyUSB0', baudrate=9600)  # open serial port
     print(f"Use instance: {ser.name}")  # check which port was really used
 
-    floor_state = ['', '']
-    temp_floor_sprite = Sprite(pyglet.resource.animation(f"1.gif"),
-                               x=50,
-                               y=50,
-                               batch=batch,
-                               group=foreground)
-    temp_floor_sprite.visible = False
+    floor_state = ['0', '0']
+    ok_list = ('1', '2', '3', '4', '5')
+    data_str = []
 
-    while True:
-        pyglet.clock.tick()
-        # обработка UART посылок from MCU
-        if ser.inWaiting() > 0:
-            # read the bytes and convert from binary array to ASCII
-            data_str = ser.read(ser.inWaiting()).decode('ascii')
+while True:
+    pyglet.clock.tick()
 
-            match data_str:
-                # Floor number updating
-                case '0' | '1' | '2' | '3' | '4' | '5':
-                    floor_state[0] = data_str
-                    if floor_state[0] is not floor_state[1] and floor_state[1] != '':
-                        if temp_floor_sprite in sprites:
-                            sprites.remove(temp_floor_sprite)
-                            temp_floor_sprite.delete()
+    # обработка UART посылок from MCU
+    if ser.inWaiting() > 0:
+        # read the bytes and convert from binary array to ASCII
+        data_str = ser.read(ser.inWaiting()).decode('ascii')
 
-                        animation = Sprite(pyglet.resource.animation(f"{floor_state[1]}_{floor_state[0]}.gif"),
-                                           batch=batch,
-                                           x=50, y=50,
-                                           group=foreground)
-                        sprites.append(animation)
-                        test_floor = floor_state[0]
+    if data_str in ok_list:
+        floor_state[0] = data_str
 
-                        @animation.event
-                        def on_animation_end():
-                            global temp_floor_sprite, animation
-                            if animation in sprites:
-                                sprites.remove(animation)
-                                animation.delete()
-                                print("Гифка закончилась!")
+        if floor_state[0] is not floor_state[1]:
+            sprites.insert(0, Sprite(pyglet.resource.animation(f"{floor_state[0]}.gif"),
+                                     x=50, y=50,
+                                     batch=batch,
+                                     group=foreground))
+        data_str = ''
+        print(floor_state)
+        floor_state[1] = floor_state[0]
 
-                            temp_floor_sprite = Sprite(image.load(f"{test_floor}.gif"),
-                                                       x=50, y=50,
-                                                       batch=batch,
-                                                       group=foreground)
-                            sprites.append(temp_floor_sprite)
-
-                    elif floor_state[0] == floor_state[1]:
-                        if temp_floor_sprite in sprites:
-                            sprites.remove(temp_floor_sprite)
-                            temp_floor_sprite.delete()
-
-                        temp_floor_sprite = Sprite(pyglet.resource.animation(f"{test_floor}.gif"),
-                                                   x=50, y=50,
-                                                   batch=batch,
-                                                   group=foreground)
-                        sprites.append(temp_floor_sprite)
-
-                    print(floor_state)
-                    floor_state[1] = floor_state[0]
-                case _:
-                    print(data_str)
-                    print("Undefined value!!\n")
-
-        # Отрисовка изображения
-        for window in pyglet.app.windows:
-            window.switch_to()
-            window.dispatch_events()
-            window.dispatch_event('on_draw')
-            window.flip()
-
+    # Отрисовка изображения
+    for window in pyglet.app.windows:
+        window.switch_to()
+        window.dispatch_events()
+        window.dispatch_event('on_draw')
+        window.flip()
