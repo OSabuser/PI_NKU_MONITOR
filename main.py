2f4d15b3efd3b69c4f05641b53adc8bc5a389feb
Dmitry Akimov <von.akimov@gmail.com>
2022-10-16 22:18:03 +0300

    STABLE_XX

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
     arrows = []
 
     for element in range(1, 6):
-        gifs.append(Sprite(pyglet.resource.animation(f"{element}.gif"), x=50, y=800, group=foreground))
+        gifs.append(Sprite(pyglet.resource.animation(f"{element}.gif"), x=50, y=900, group=foreground))
 
-    arrows.append(Sprite(image.load('UP.png'), x=75, y=250, group=foreground))
-    arrows.append(Sprite(image.load('DOWN.png'), x=75, y=250, group=foreground))
+    arrows.append(Sprite(image.load('UP.png'), x=75, y=350, group=foreground))
+    arrows.append(Sprite(image.load('DOWN.png'), x=75, y=350, group=foreground))
 
     animation = gifs[0]
     arrow_img = arrows[0]
